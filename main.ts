c086dbee1f09a76b23222a61ce08a6833819ced0
hsmtkk <hsmtkk@gmail.com>
2022-12-04 08:22:40 +0900

    cdktf manifest

---
+++ main.ts
@@ -0,0 +1,74 @@
+// Copyright (c) HashiCorp, Inc
+// SPDX-License-Identifier: MPL-2.0
+import { Construct } from "constructs";
+import { App, TerraformStack, CloudBackend, NamedCloudWorkspace, TerraformAsset, AssetType } from "cdktf";
+import * as google from '@cdktf/provider-google';
+import * as path from 'path';
+
+const project = 'supreme-adventure-370523';
+const region = 'asia-northeast1';
+//const repository = 'supreme-adventure';
+
+class MyStack extends TerraformStack {
+  constructor(scope: Construct, id: string) {
+    super(scope, id);
+
+    new google.provider.GoogleProvider(this, 'google', {
+      project,
+      region,
+    });
+
+    const source_bucket = new google.storageBucket.StorageBucket(this, 'source-bucket', {
+      location: region,
+      name: `source-${project}`,
+    });
+
+    const source_asset = new TerraformAsset(this, 'source-asset', {
+      path: path.resolve('orig'),
+      type: AssetType.ARCHIVE,
+    });
+
+    const source_object = new google.storageBucketObject.StorageBucketObject(this, 'source-object', {
+      bucket: source_bucket.name,
+      source: source_asset.path,
+      name: `${source_asset.assetHash}.zip`,
+    });
+
+    const nodejs_bq_function = new google.cloudfunctions2Function.Cloudfunctions2Function(this, 'nodejs-bq-function', {
+      buildConfig: {
+        entryPoint: 'helloBigQuery',
+        runtime: 'nodejs',
+        source: {
+          storageSource: {
+            bucket: source_bucket.name,
+            object: source_object.name,
+          },
+        },
+      },
+      location: region,
+      name: 'nodejs-bq-function',
+    });
+
+    const noauth_data = new google.dataGoogleIamPolicy.DataGoogleIamPolicy(this, 'noauth-data', {
+      binding: [{
+        members: ['allUsers'],
+        role: 'roles/run.invoker',        
+      }],
+    });
+
+    new google.cloudRunServiceIamPolicy.CloudRunServiceIamPolicy(this, 'cloudrun-noauth', {
+      location: region,
+      policyData: noauth_data.policyData,
+      service: nodejs_bq_function.name,
+    });
+  }
+}
+
+const app = new App();
+const stack = new MyStack(app, "supreme-adventure");
+new CloudBackend(stack, {
+  hostname: "app.terraform.io",
+  organization: "hsmtkkdefault",
+  workspaces: new NamedCloudWorkspace("supreme-adventure")
+});
+app.synth();
